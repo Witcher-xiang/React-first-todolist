d664ce81520e4f3b95b24674e556f6755656f45f
Witcher-xiang <1214334086@qq.com>
2021-12-17 22:20:59 +0800

    [Add] 增加内容

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 // 上个版本 使用的是class App extends React.Component
 // 只要是在这个return{}中 <>标签都是jsx的语法。想要让他运行就得引入react进行编译识别
+// 多加点点等下
 function App() {
   return (
     <div className="App">
